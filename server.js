cb4d35f335586bedc1d23c064139806416f6d36b
Village01 <62046199+Village01@users.noreply.github.com>
2020-03-29 16:05:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,47 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 8888;
 
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUi=require('swagger-ui-express');
+
+const swaggerOptions={
+    swaggerDefinition: {
+        info: {
+            title: 'My name',
+            description: "Hello World",
+            version: "1.0.0",
+            contact: {
+                name: "Hoàng Minh Phát",
+                email: "17520876@gm.edu.vn",
+            },
+            servers: ["village01.herokuapp.com"]
+        }
+    },
+    apis: ["server.js"]
+};
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/apidocs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: ...
+ *      description: ...
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          '200': 
+ *              description: ....
+ * /phat:
+ *  get:
+ *      summary: ...
+ *      responses: 
+ *          '200':
+ *              description: ok
+ *     
+ */
+
 app.get('/',(req, res) => {
     res.send({
         Name: 'Hoang Minh Phat', 
